a00ef3b786b496dcdd5402cc4563bd26c71f7fec
Andrey <afuentes40060@ufide.ac.cr>
2025-07-11 14:12:50 -0600

    Implementacion de la logica de la clase Cuenta y "getters and setters" + constructor de la clase Cliente.

--- src/main/java/model/Cliente.java
+++ src/main/java/model/Cliente.java
@@ -16,6 +16,63 @@ public class Cliente implements Serializable {
     private String telefono;
     private String email;
 
+    public Cliente(int idCliente, String nombre, String apellido, String direccion, String telefono, String email) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public void registrarCliente() { }
     public void actualizarDatos() { }
     public String obtenerInformacion() { return null; }
--- src/main/java/model/Cuenta.java
+++ src/main/java/model/Cuenta.java
@@ -5,6 +5,8 @@
 package model;
 import java.io.Serializable;
 import java.util.List;
+import java.util.ArrayList;
+import exception.FondosInsuficientesException;
 /**
  *
  * @author XPC
@@ -14,14 +16,88 @@ public class Cuenta implements Serializable {
     private double balance;
     private String PIN;
     private Cliente cliente;
-    private List<model.Transaccion> historialTransacciones;
+    private List<Transaccion> historialTransacciones = new ArrayList<>();
 
-    public void depositar(double monto) { }
-    public void retirar(double monto) { }
-    public void transferir(double monto, Cuenta cuentaDestino) { }
-    public boolean validarPIN(String pin) { return false; }
-    public void imprimirComprobante(Transaccion transaccion) { }
+    public Cuenta(String numCuenta, Cliente cliente, String PIN, double saldoInicial) {
+        this.numCuenta = numCuenta;
+        this.cliente = cliente;
+        this.PIN = PIN;
+        this.balance = saldoInicial;
+    }
+
+    public String getNumCuenta() {
+        return numCuenta;
+    }
+
+    public void setNumCuenta(String numCuenta) {
+        this.numCuenta = numCuenta;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getPIN() {
+        return PIN;
+    }
+
+    public void setPIN(String PIN) {
+        this.PIN = PIN;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+   
+    public void depositar(double monto) {
+        if (monto <= 0) {
+            throw new IllegalArgumentException("Monto debe ser mayor que cero");
+        }
+        balance += monto;
+    }
+    
+    public void retirar(double monto) throws FondosInsuficientesException { 
+        if (monto <= 0) {
+            throw new IllegalArgumentException("Monto debe ser mayor que cero");
+        }
+        if (monto > balance) {
+            throw new FondosInsuficientesException("Saldo insuficiente");
+        }
+        balance -= monto;   
+    }
+    
+    public void transferir(double monto, Cuenta cuentaDestino) throws FondosInsuficientesException {
+        if (cuentaDestino == null) {
+            throw new IllegalArgumentException("Cuenta destino inválida");
+        }
+        this.retirar(monto);
+        cuentaDestino.depositar(monto);              
+    }
+    
+    public boolean validarPIN(String pin) { 
+        return PIN.equals(pin); 
+    }
+    
+    public void imprimirComprobante(Transaccion transaccion) {
+        // llama a Comprobante.generar() e imprimir()         
+    }
     @Override
-    public String toString() { return super.toString(); }
-    public List<Transaccion> getHistorialTransacciones() { return null; }
+    public String toString() { 
+        return String.format(
+            "Cuenta[%s] - Saldo: %.2f - Titular: %s %s",
+            numCuenta, balance, cliente.getNombre(), cliente.getApellido());
+
+    }
+    
+    public List<Transaccion> getHistorialTransacciones() { 
+        return historialTransacciones; 
+    }    
 }
